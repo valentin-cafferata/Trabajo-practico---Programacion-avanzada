d4128393c5bd9d4134a7159c173882134aeaeec1
pablitohot <valentincafferata11@gmail.com>
2026-09-08 23:18:11 -0300

    pasé todo porque me rompen las bolitas y orne y tobi no hacen NADA

--- TP/index.php
+++ TP/index.php
@@ -10,8 +10,14 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link href="css/navbar.css" rel="stylesheet">
+
+    
+    
+  </head>
+
     <style>
-      body {
+       body {
         /* Reemplaza con la ruta real de tu imagen */
         background-image: url('img/Maxwell-2.jpg'); 
         background-repeat: repeat;
@@ -31,33 +37,35 @@ session_start();
           background-position: -1000px 0; 
         }
       }
+      
     </style>
-  </head>
-
+  
   <body>
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
   
-    <nav class="navbar navbar-expand-lg bg-primary">
+    <nav class="navbar navbar-expand-lg">
       <div class="container-fluid">
         <a class="navbar-brand" href="#">
           <img src="img/logo.png" alt="Logo Marketplace" width="50" height="50" class="d-inline-block align-text-top">
         </a>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
               <a class="nav-link active" aria-current="page" href="index.php">Home</a>
             </li>
-          </ul>
+        </ul>
           
           <div class="d-flex align-items-center">
             <?php if (isset($_SESSION['usuario'])): ?>
               <span class="text-light fw-bold me-3">
                 <?php echo htmlspecialchars($_SESSION['usuario']); ?>
               </span>
-              <a href="logout.php" class="btn btn-outline-light btn-sm">Salir</a>
+              <a class="nav-link active" aria-current="page" href="logout.php">Salir</a>
+              <!-- <a href="logout.php" class="btn btn-outline-light btn-sm">Salir</a> -->
             <?php else: ?>
-              <a href="login.php" class="btn btn-light">Login</a>
+              <a class="nav-link active" aria-current="page" href="login.php">Login</a>
             <?php endif; ?>
           </div>
 
