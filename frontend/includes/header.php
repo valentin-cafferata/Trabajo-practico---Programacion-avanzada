50e7bbd99f42f2fa7ff2baf4e0e342664f3da096
pablitohot <valentincafferata11@gmail.com>
2026-09-21 20:13:04 -0300

    agruegé un icono de perfil y tambien cambié el color de cuando pasas el raton sobre los botones del navbar a uno mas lindo

--- frontend/includes/header.php
+++ frontend/includes/header.php
@@ -36,9 +36,10 @@ $auth = $desdeBackend ? '' : '../backend/';
           </li>
         </ul>
         <div class="d-flex align-items-center">
+          <img src="<?php echo $front; ?>assets/img/profile.png" alt="Perfil" width="40" height="40" class="rounded-circle me-3">
           <?php if (isset($_SESSION['usuario'])): ?>
             <span class="text-light fw-bold me-3">
-              Logueado como: <?php echo htmlspecialchars($_SESSION['usuario']); ?>
+              <?php echo htmlspecialchars($_SESSION['usuario']); ?>
             </span>
             <a href="<?php echo $auth; ?>logout.php" class="btn btn-outline-light btn-sm">Salir</a>
           <?php elseif ($paginaActual !== 'login.php'): ?>
